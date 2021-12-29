7eeb8a4187cd5101a2d3c68e86b0fc9ca344605b
Michael Zürcher <michzuerch@gmail.com>
2021-12-29 13:12:00 +0100

    images and titles

--- src/lib/Components/Lightbox/imageData.js
+++ src/lib/Components/Lightbox/imageData.js
@@ -1,51 +1,104 @@
 export const images = [
 	{
 		id: 0,
-		name: 'Green Nature',
-		imgurl:
-			'https://1.bp.blogspot.com/-up45dJ1vwEE/UMAw3fWxviI/AAAAAAAAOkE/AKrDgMDkLI0/s1600/Green+Nature+Wallpapers.jpg',
-		attribution: 'wallpapers-xs.blogspot.com'
+		name: 'Porsche',
+		imgurl: '/Autos/Gallery/1.jpg',
+		attribution: 'Porsche'
 	},
 	{
 		id: 1,
-		name: 'Red Nature',
-		imgurl:
-			'https://2.bp.blogspot.com/_N_mOB63qPaE/TTGNvMO6zZI/AAAAAAAARoU/LX2QUhrTmoQ/s1600/Desktop-Nature-Wallpaper.jpg',
-		attribution: 'animalszooguru.blogspot.com'
+		name: 'Classic car',
+		imgurl: '/Autos/Gallery/2.jpg',
+		attribution: 'Classic car'
 	},
 	{
 		id: 2,
-		name: 'Path',
-		imgurl:
-			'https://2.bp.blogspot.com/-I91kB3xYppQ/TZDPvc-ilzI/AAAAAAAABvw/JQBz0_8E_I4/s1600/Natural+Scenes+%25286%2529.jpg',
-		attribution: 'hakeem786.blogspot.com'
+		name: 'Ford',
+		imgurl: '/Autos/Gallery/3.jpg',
+		attribution: 'Ford'
 	},
 	{
 		id: 3,
-		name: 'Country Road',
-		imgurl:
-			'https://2.bp.blogspot.com/-JxEm57kYpms/UMAvQULqnuI/AAAAAAAAOis/vVuC7CIN-BU/s1600/Green+Nature+Wallpapers+02.jpg',
-		attribution: 'wallpapers-xs.blogspot.com'
+		name: 'Ford',
+		imgurl: '/Autos/Gallery/4.jpg',
+		attribution: 'Ford'
 	},
 	{
 		id: 4,
-		name: 'Throughway',
-		imgurl:
-			'https://1.bp.blogspot.com/-UjjdMoyW55E/TuRwkTMengI/AAAAAAAABCU/Qot38EiUZDU/s1600/Nature+Photo+Gallery.jpg',
-		attribution: 'onlinenaturalphoto.blogspot.com'
+		name: 'Classic car',
+		imgurl: '/Autos/Gallery/5.jpg',
+		attribution: 'Classic car'
 	},
 	{
 		id: 5,
-		name: 'Golden Pond',
-		imgurl:
-			'https://3.bp.blogspot.com/-DWMXiqFQQsM/Tn2j9xWAwyI/AAAAAAAAAE0/XpQA7OtDGTw/s1600/amazing_nature_pictures+4.jpg',
-		attribution: 'crazy-frankenstein.com'
+		name: 'Porsche',
+		imgurl: '/Autos/Gallery/6.jpg',
+		attribution: 'Porsche'
 	},
 	{
 		id: 6,
-		name: 'Sunflower',
-		imgurl:
-			'https://s19499.pcdn.co/wp-content/uploads/2019/07/Poolesville-Sunflowers-July-4-2019-DSC08529.jpg',
-		attribution: 'mymcmedia.org'
+		name: 'Hyundai',
+		imgurl: '/Autos/Gallery/7.jpg',
+		attribution: 'Hyundai'
+	},
+	{
+		id: 7,
+		name: 'BMW Cabrio',
+		imgurl: '/Autos/Gallery/8.jpg',
+		attribution: 'BMW Cabrio'
+	},
+	{
+		id: 8,
+		name: 'Mercedes Cabrio',
+		imgurl: '/Autos/Gallery/9.jpg',
+		attribution: 'Mercedes Cabrio'
+	},
+	{
+		id: 9,
+		name: 'Renault',
+		imgurl: '/Autos/Gallery/10.jpg',
+		attribution: 'Renault'
+	},
+	{
+		id: 10,
+		name: 'Mercedes',
+		imgurl: '/Autos/Gallery/11.jpg',
+		attribution: 'Mercedes'
+	},
+	{
+		id: 11,
+		name: 'Ford',
+		imgurl: '/Autos/Gallery/12.jpg',
+		attribution: 'Ford'
+	},
+	{
+		id: 12,
+		name: 'Mercedes',
+		imgurl: '/Autos/Gallery/13.jpg',
+		attribution: 'Mercedes'
+	},
+	{
+		id: 13,
+		name: 'VW Cabrio',
+		imgurl: '/Autos/Gallery/14.jpg',
+		attribution: 'VW Cabrio'
+	},
+	{
+		id: 14,
+		name: 'Classic car',
+		imgurl: '/Autos/Gallery/15.jpg',
+		attribution: 'Classic car'
+	},
+	{
+		id: 15,
+		name: 'BMW',
+		imgurl: '/Autos/Gallery/16.jpg',
+		attribution: 'BMW'
+	},
+	{
+		id: 16,
+		name: 'Classic car',
+		imgurl: '/Autos/Gallery/17.jpg',
+		attribution: 'Classic car'
 	}
 ];
