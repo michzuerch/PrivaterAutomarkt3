a89643cca02d8612eac0c679bbef2756906af0e3
Michael Zürcher <michzuerch@gmail.com>
2021-12-29 12:54:39 +0100

    Lightbox working

---
+++ src/lib/Components/Lightbox/Slide.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let slideNo;
+	export let totalSlides;
+	export let image;
+	export let altTag;
+	export let attr;
+</script>
+
+<!-- Full-width images with number text -->
+<div class="mySlides">
+	<div class="numbertext">{slideNo} / {totalSlides}</div>
+	<img src={image} alt={altTag} title={attr} />
+</div>
+
+<style>
+	/* Number text (1/3 etc) */
+	.numbertext {
+		color: #ddd;
+		font-size: 0.9rem;
+		padding: 8px 12px;
+		text-shadow: 1px 1px black;
+		position: absolute;
+		top: 0;
+	}
+
+	img {
+		width: 100%;
+	}
+</style>
--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -1,5 +1,48 @@
-<script lang="ts">
-	import Carousel from '$lib/Components/Carousel.svelte';
+<script>
+	import Lightbox from '$lib/Components/Lightbox/Lightbox.svelte';
+	import ImageDisplay from '$lib/Components/Lightbox/ImageDisplay.svelte';
+
+	let modalOpen = false;
+	// $: console.log(modalOpen);
+
+	let imageShowingIndex = 0;
+
+	const handleImgClick = (e) => {
+		imageShowingIndex = Number(e.target.id);
+		modalOpen = true;
+	};
 </script>
 
-<Carousel />
+<!-- The Modal/Lightbox -->
+{#if modalOpen}
+	<Lightbox on:click={() => (modalOpen = false)} {imageShowingIndex} />
+{:else}
+	<ImageDisplay on:click={handleImgClick} />
+{/if}
+
+<!-- Button to open Lightbox -->
+<section>
+	<button on:click={() => (modalOpen = true)}> Lightbox </button>
+</section>
+
+<style>
+	section {
+		width: 100%;
+		text-align: center;
+		margin-top: 5%;
+	}
+
+	button {
+		background-color: #000;
+		color: white;
+		padding: 10px 20px;
+		box-shadow: 0 2px 3px black;
+		cursor: pointer;
+	}
+
+	button:active {
+		background-color: #555;
+		color: white;
+		box-shadow: none;
+	}
+</style>
